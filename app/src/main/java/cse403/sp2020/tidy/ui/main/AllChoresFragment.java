a3d0821d52fb88350389008fddcbc61c8ce8e9c7
Kcee Landon <landonkcee@gmail.com>
2020-05-02 16:12:52 -0700

    added basic tab activity for main screen

---
+++ app/src/main/java/cse403/sp2020/tidy/ui/main/AllChoresFragment.java
@@ -0,0 +1,24 @@
+package cse403.sp2020.tidy.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import cse403.sp2020.tidy.R;
+
+public class AllChoresFragment extends Fragment {
+    private static final String TAG = "AllChoresFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.allchores_fragment, container, false);
+
+        return view;
+    }
+}
